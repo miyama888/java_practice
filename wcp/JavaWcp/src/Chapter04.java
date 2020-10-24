797e93c20f10cfed006ed27a34d88eb90cfbd6d2
深山　俊 <macnel@fukayamashunnoMacBook-Pro.local>
2020-10-24 23:26:17 +0900

    04-complete

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "深山";
+		System.out.println(name);
+		name = "ステイサム";
+		System.out.println(name);
+		final int LUC = 7;
+		System.out.println(LUC);
+	}
+}
